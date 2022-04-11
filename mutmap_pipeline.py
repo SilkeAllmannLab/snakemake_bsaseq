0d6099d2302d5152185b5193a77fecc49771de45
Marc Galland <m.galland@uva.nl>
2022-04-11 15:36:07 +0200

    fix gzip mistake

--- mutmap_pipeline.py
+++ mutmap_pipeline.py
@@ -190,7 +190,8 @@ for sample in samples:
         vcf_decompress_cmd = "gzip -d " + RESULT_DIR + sample + "/30_vcf/mutmap.vcf.gz"
         subprocess.call(vcf_decompress_cmd, shell=True)
 
-        snpeff_cmd = "gzip --keep -d -c " + RESULT_DIR + sample + "/30_vcf/mutmap.vcf.gz | snpEff ann " + "-o " + SNPEFF_FORMAT + " -csvStats -onlyProtein -v " + SNPEFF_DB + " " + RESULT_DIR + sample + "/30_vcf/mutmap.vcf > " + RESULT_DIR + sample + "/mutmap_annotated.vcf"   
+        # Annotate SNPs using snpEff
+        snpeff_cmd = "snpEff ann " + "-o " + SNPEFF_FORMAT + " -csvStats -onlyProtein -v " + SNPEFF_DB + " " + RESULT_DIR + sample + "/30_vcf/mutmap.vcf > " + RESULT_DIR + sample + "/mutmap_annotated.vcf"   
         subprocess.call(snpeff_cmd, shell=True)    
 
 
